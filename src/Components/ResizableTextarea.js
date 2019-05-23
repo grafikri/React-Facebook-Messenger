6cd2d9004f38e4a679549a625a0631c484185ced
Serhan Coşkun <grafikri@gmail.com>
2019-05-23 19:56:19 +0300

    Dynamic text input designed for chat

---
+++ src/Components/ResizableTextarea.js
@@ -0,0 +1,79 @@
+/**
+ * 
+ * Part of this code has been taken from https://codepen.io/Libor_G/pen/eyzwOx
+ * 
+ */
+import './ResizableTextarea.css'
+import React from 'react'
+
+class ResizableTextarea extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      rows: 1,
+      minRows: 1,
+      maxRows: 5,
+    }
+
+
+    this.handleOnKeyDown = this.handleOnKeyDown.bind(this)
+    this.handleEnterKeyTriggered = this.handleEnterKeyTriggered.bind(this)
+  }
+
+  handleChange = (event) => {
+    const textareaLineHeight = 24
+    const { minRows, maxRows } = this.state
+
+    const previousRows = event.target.rows
+    event.target.rows = minRows // reset number of rows in textarea 
+
+    const currentRows = ~~(event.target.scrollHeight / textareaLineHeight)
+
+    if (currentRows === previousRows) {
+      event.target.rows = currentRows
+    }
+
+    if (currentRows >= maxRows) {
+      event.target.rows = maxRows
+      event.target.scrollTop = event.target.scrollHeight
+    }
+
+    this.setState({
+      value: event.target.value,
+      rows: currentRows < maxRows ? currentRows : maxRows,
+    })
+  }
+
+  handleOnKeyDown(event) {
+    if (event.keyCode == 13 && event.shiftKey == false) {
+      event.preventDefault();
+      if (this.state.value.trim() == '') { return }
+
+      this.handleEnterKeyTriggered()
+    }
+  }
+
+  handleEnterKeyTriggered() {
+    this.props.handleSubmit(this.state.value.trim())
+    this.setState({ value: '' })
+  }
+
+  render() {
+    return (
+      <textarea
+        rows={this.state.rows}
+        value={this.state.value}
+        placeholder={'Bir mesaj yazın...'}
+        className={'Resizable-Textarea'}
+        onChange={this.handleChange}
+        onKeyDown={this.handleOnKeyDown}
+      />
+    )
+  }
+}
+
+
+
+
+export default ResizableTextarea
