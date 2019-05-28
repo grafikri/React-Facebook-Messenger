0fa14792506d47df02f8176bd3de95677b3affca
Serhan Coşkun <grafikri@gmail.com>
2019-05-28 03:00:18 +0300

    sender chat buble creating

--- src/Components/MessageListItemSender.js
+++ src/Components/MessageListItemSender.js
@@ -8,7 +8,19 @@ class MessageListItemSender extends React.Component {
 
 		return (
 			<div className="Message-List-Item-Sender">
-				<div className="Bubbles">Bubbles</div>
+				<div className="Bubbles">
+					<p>
+						dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem
+						Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı
+						oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden
+						beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz
+						yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek
+						değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum
+						pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın
+						zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü
+						yayıncılık yazılımları ile popüler olmuştur.
+					</p>
+				</div>
 				<div className="Photo">
 					<img src={url} alt="" />
 				</div>
